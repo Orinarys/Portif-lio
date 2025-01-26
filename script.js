9fde29b0ea702acf0e6fd831c9760005f8cb38ce
kevinDV <147874522+Orinarys@users.noreply.github.com>
2025-01-26 17:39:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// Seleciona o ícone do menu e a barra de navegação
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
-// Adiciona um ouvinte de eventos para o clique no ícone do menu
 menuIcon.addEventListener('click', () => {
-    // Alterna a classe 'bx-x' para o ícone do menu, mudando sua aparência
     menuIcon.classList.toggle('bx-x');
-    // Alterna a classe 'active' para a barra de navegação, mostrando ou ocultando-a
     navbar.classList.toggle('active');
 });
